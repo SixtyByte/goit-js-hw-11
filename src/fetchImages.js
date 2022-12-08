134c5a93b9bf1c270874c3acfb4d11ddd5d47d5f
SixtyByte <valentinvodopyan@ukr.net>
2022-12-08 21:54:23 +0200

    1

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,12 +1,14 @@
 
 import { Notify } from "notiflix" 
-
+import axios from "axios"
 export default fetchImages
 
-function fetchImages(name){const base_url=`https://restcountries.com/v2/name/`
-    return fetch(`${base_url}${name}?fields=name,capital,population,flags,languages`).then(resp=>{if(!resp.ok){
-    throw new Error(Notify.failure(`Oops, there is no country with that name`))
+function fetchImages(name){const base_url=`https://pixabay.com/api/?key=`
+const API_KEY=`31935683-a10b01c36d3de5d3933439cde`
+    return fetch(`${base_url}${API_KEY}&q=${name}&image_type=photo&orientation =horizontal&safesearch =true`).then(resp=>{if(!resp.ok){
+    throw new Error(Notify.failure(`No image`))
       }
+      console.log(resp)
       return resp.json()
 }).catch(err=>console.error(err))
 
--- src/index.js
+++ src/index.js
@@ -1,43 +1,51 @@
+import axios from 'axios';
 import debounce from 'lodash.debounce';
 import './css/styles.css';
-import {Notify} from 'notiflix';
+import {Notify} from 'notiflix'
 import fetchImages from './fetchImages.js';
 
 
-const form=document.querySelector(`#search-box`)
-const listOfCoutrys=document.querySelector(`.country-list`)
-const countryInfo=document.querySelector(`.country-info`)
+const form=document.querySelector(`#search-form`)
+const photoCard=document.querySelector(`.photo-card`)
 const DEBOUNCE_DELAY = 300;
 
 
 
-form.addEventListener(`input`, debounce(onSerch,DEBOUNCE_DELAY) )
+form.addEventListener(`submit`, onSerch )
 
-function onSerch(){
-const serchInput=form.value.trim();
-if (serchInput==""){clearMarkupAll()}
-fetchImages(serchInput).then(data=>
-{if(data==undefined){console.log("Немає такої країни ")}
-else if(data.length===1){createCountryInfo(data); listOfCoutrys.innerHTML=``}
-else if(data.length>=2 && data.length<=10){createList(data);countryInfo.innerHTML = '';}
-else if (data.length>10){Notify.failure(`Too many matches found. Please enter a more specific name.`)}
-});
+function onSerch(event){
+event.preventDefault();
+const formEl=event.currentTarget.elements;
+let text=formEl.searchQuery.value
+console.log(text)
+if (text==""){clearMarkupAll()}
+fetchImages(text).then(data=>
+{console.log(data)}
+
+);
 
 
-}
+
 
 
 function clearMarkupAll(){ countryInfo.innerHTML = ''; listOfCoutrys.innerHTML=``}
 
 function createCountryInfo(arr){ 
-const markup=arr.map(item=>`<ul class="country-info-list" style="list-style-type: none">
-<li><img src="${item.flags.svg}" alt="flag" height =50px,width=50px><h1> ${item.name}</h1>
-<h2><b>Capital:</b>${item.capital}</h2>
-<h2><b>Population:</b>${item.population}</h2>
-<h2><b>Language:</b>${item.languages.map(({name})=>`<span>${name}</span>`).join(``)}</h2>
-</li>
-</ul>`).join('');
-countryInfo.innerHTML =markup 
+const markup=arr.map(item=>`<img src="" alt="" loading="lazy" />
+<div class="info">
+  <p class="info-item">
+    <b>Likes</b>
+  </p>
+  <p class="info-item">
+    <b>Views</b>
+  </p>
+  <p class="info-item">
+    <b>Comments</b>
+  </p>
+  <p class="info-item">
+    <b>Downloads</b>
+  </p>
+</div>`).join('');
+photoCard.innerHTML =markup 
 }
-function createList (arr){const listMarkup=arr.map(item=>`<li class="country-result"><img src="${item.flags.svg}" alt="" height =10px,width=10px>${item.name}</li>`).join(``);
-listOfCoutrys.innerHTML=listMarkup;}
+}
