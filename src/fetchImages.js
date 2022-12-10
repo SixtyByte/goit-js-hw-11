02c423609d5d0ef2b4ddcc305f737ad709fe0d9f
SixtyByte <valentinvodopyan@ukr.net>
2022-12-11 00:43:46 +0200

    1

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,11 +1,1 @@
 
-import { Notify } from "notiflix" 
-import axios from "axios"
-export default fetchImages
-
-function fetchImages(name){const base_url=`https://pixabay.com/api/?key=`
-const API_KEY=`31935683-a10b01c36d3de5d3933439cde`
-return axios.get(`${base_url}${API_KEY}&q=${name}&image_type=photo&orientation =horizontal&safesearch =true`).then(resp=>{return resp;}).catch(Notify.failure(`Oops, SomeError`))
-
-
-}
--- src/index.js
+++ src/index.js
@@ -1,49 +1,72 @@
 import axios from 'axios';
-import debounce from 'lodash.debounce';
 import './css/styles.css';
 import {Notify} from 'notiflix'
-import fetchImages from './fetchImages.js';
+let total;
+
 
 
 const form=document.querySelector(`#search-form`)
-const photoCard=document.querySelector(`.photo-card`)
+const gallery=document.querySelector(`.gallery`)
+const loadMore=document.querySelector(`.load-more`)
+let pages=1;
+loadMore.hidden=true
+console.log(loadMore)
+loadMore.addEventListener(`click`, onload);
 
 
 
 
-form.addEventListener(`submit`, onSerch )
+form.addEventListener(`click`, onSerch )
 
 function onSerch(event){
+pages=1
 event.preventDefault();
 const formEl=event.currentTarget.elements;
 let text=formEl.searchQuery.value
-
+if (text=='') {return Notify.failure("Напиши щось")
+  
+}
 if (text==""){clearMarkupAll()}
-fetchImages(text).then(data=>{console.log(data)}
+fetchImages(text,1).then(data=>{createGallery(data);  if(data.data.hits.length<40&data.data.hits.length>1){ loadMore.disabled=true; Notify.failure(`We're sorry, but you've reached the end of search results.".`);}
+else if(data.data.hits.length>=40){loadMore.hidden=false;} else if (data.data.hits.length==0) {Notify.failure(`Oops, there no image`);loadMore.hidden=true;
+}}
 
 );
 
 }
 
+function onload(){pages+=1; fetchImages(form.elements.searchQuery.value).then(createGallery).then(total+=data.data.hits.length);
+console.log(total) }
 
-
-function clearMarkupAll(){ countryInfo.innerHTML = ''; listOfCoutrys.innerHTML=``}
+function clearMarkupAll(){ photoCard.innerHTML = ''}
 
 function createGallery(arr){ 
-const markup=arr.map(item=>`<img src="${item.webformatURL}" alt="" loading="lazy" />
+console.log(arr)
+const markup=arr.data.hits.map(item=>`<div class="photo-card">
+<img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" class="image-item" />
 <div class="info">
   <p class="info-item">
-    <b>Likes</b>
+    <b>Likes ${item.likes}</b>
   </p>
   <p class="info-item">
-    <b>Views</b>
+    <b>Views ${item.views}</b>
   </p>
   <p class="info-item">
-    <b>Comments</b>
+    <b>Comments ${item.comments}</b>
   </p>
   <p class="info-item">
-    <b>Downloads</b>
+    <b>Downloads ${item.downloads}</b>
   </p>
+</div>
 </div>`).join('');
-photoCard.innerHTML =markup 
+gallery.innerHTML =markup 
 }
+
+
+function fetchImages(name,page){const base_url=`https://pixabay.com/api/?key=`
+  const API_KEY=`31935683-a10b01c36d3de5d3933439cde`
+  return axios.get(`${base_url}${API_KEY}&q=${name}&image_type=photo&orientation =horizontal&safesearch =true&per_page=40&page=${pages}`).then(resp=>{return resp;}).catch(err=>console.error(err))
+  
+  }
+
+
